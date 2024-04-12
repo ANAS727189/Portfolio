7ce1f0cca78051d936fcc097822f6a1b1ca15620
ANAS727189 <144107389+ANAS727189@users.noreply.github.com>
2024-04-12 14:57:54 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+var typed = new Typed('#element', {
+    strings: ["Web developer .", "CP enthuthiast .", "Blockchain developer .",  "Python Developer .", "AI/ML Learner ."],
+    typeSpeed: 50,
+    startDelay: 20,
+    loop: true,
+    loopCount: Infinity,
+  });
+
+
+
